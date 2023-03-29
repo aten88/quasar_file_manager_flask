7719c0075ce230283bff8811a39f0cd368fd0bca
aten88 <virtuoso_ten@mail.ru>
2023-03-29 15:37:02 +0300

    Need make 2 tests

--- configuration.py
+++ configuration.py
@@ -1,8 +1,7 @@
 INDEX_URL = 'http://127.0.0.1:4567/'
-LIST_URL = 'http://127.0.0.1:4567/files/get/list'
-TYPE_URL = 'http://127.0.0.1:4567/files/get/'
+GET_URL = 'http://127.0.0.1:4567/files/get/'
 TEST_EXTENSIONS = 'txt', 'csv', 'json', 'xlsx', 'jpg', 'png'
-TEST_FILENAMES = (
+TEST_URL_FILENAMES = (
     'txt/Тестовый документ',
     'csv/Тестовый',
     'jpg/Тестовое изображение'
--- tests/test_server.py
+++ tests/test_server.py
@@ -9,10 +9,9 @@ from app.main.quasar_junior_flask_server import (
 
 from configuration import (
     INDEX_URL,
-    LIST_URL,
-    TYPE_URL,
+    GET_URL,
     TEST_EXTENSIONS,
-    TEST_FILENAMES
+    TEST_URL_FILENAMES
 )
 
 
@@ -26,22 +25,21 @@ def test_extension():
 def test_index():
     response = requests.get(url=INDEX_URL)
     assert response.status_code == HTTPStatus.OK, 'Ошибка сетевого запроса'
-    # assert redirect
 
 
-def test_request_data_type():
-    response = requests.get(url=LIST_URL)
+def test_request_list_files():
+    response = requests.get(url=GET_URL + '/list')
     recieved_data = response.json()
-
+    print(recieved_data)
     assert response.status_code == HTTPStatus.OK, 'Ошибка сетевого запроса'
-    assert type(recieved_data) == list, (
+    assert isinstance(recieved_data, list) is True, (
         'Тип полученных данных не является списком'
     )
 
 
 def test_request_extension():
     for extention in TEST_EXTENSIONS:
-        response = requests.get(url=TYPE_URL+extention)
+        response = requests.get(url=GET_URL+extention)
         assert response.status_code == HTTPStatus.OK, 'Ошибка расширения в URL'
         assert (extention in ALLOWED_EXTENSIONS) is True, (
             'Переданное расширение не соответствует списку разрешенных'
@@ -49,9 +47,14 @@ def test_request_extension():
 
 
 def test_request_extension_file():
-    for file in TEST_FILENAMES:
-        response = requests.get(url=TYPE_URL + file)
+    for file in TEST_URL_FILENAMES:
+        response = requests.get(url=GET_URL + file)
         assert response.status_code == HTTPStatus.OK, (
             'Ошибка имени файла в URL'
         )
 # assert Проверка файла в хранилище
+
+
+# def test_create_file():
+#     response = requests.get(url='http://127.0.0.1:4567/files/create')
+#     assert response.status_code == HTTPStatus.OK
