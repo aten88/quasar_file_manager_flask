866bcb6ff635b35228964671ab7138c0d6a54d4d
aten88 <virtuoso_ten@mail.ru>
2023-03-30 13:53:58 +0300

    All checked ready to review

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,14 @@ from flask.testing import FlaskClient
 from app.main.quasar_junior_flask_server import app
 from configuration import TEST_FILES_DIR, TEST_EXTENSIONS
 
+# Материал по работе с фикстурами взят из этих статей:
+# https://ru.hexlet.io/courses/python-advanced-testing/lessons/files/theory_unit
+# https://docs.pytest.org/en/7.1.x/how-to/tmp_path.html#the-tmp-path-fixture
+
 
 @pytest.fixture()
 def app_test():
+    """ Create test app. """
     app.config.update({
         "TESTING": True,
         "UPLOAD_FOLDER": TEST_FILES_DIR
@@ -18,6 +23,7 @@ def app_test():
 
 @pytest.fixture()
 def client(app_test) -> FlaskClient:
+    """ Create test client. """
     return app_test.test_client()
 
 
@@ -28,6 +34,7 @@ def runner(app_test):
 
 @pytest.fixture(scope='session', autouse=True)
 def return_filename():
+    """ Create test directory and test files. """
     if TEST_FILES_DIR.exists():
         for file in TEST_FILES_DIR.iterdir():
             file.unlink()
@@ -38,5 +45,3 @@ def return_filename():
         file_path.touch(exist_ok=True)
         with file_path.open('w', encoding='utf-8') as f:
             f.write(f'Тест.{extension}')
-
-# Добавить DOCSTRINGS и пояснения
